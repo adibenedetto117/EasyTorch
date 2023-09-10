153d350b4a5cb194e5d7834530ded7e90c87f866
adibenedetto117 <75105824+adibenedetto117@users.noreply.github.com>
2023-09-10 13:10:03 -0500

    fixed structure

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='fastpytorch',
+    name='quicktorch',
     version='0.0.1',
     author='Anthony DiBenedetto and David Abrutis',
     author_email='adibenedetto117@gmail.com',
     description='Fast and easy wrapper for PyTorch making it easy to use for beginners',
-    url='https://github.com/adibenedetto117/fastpytorch',
+    url='https://github.com/adibenedetto117/quicktorch',
     packages=setuptools.find_packages(),
     classifiers=[
         'Programming Language :: Python :: 3',
