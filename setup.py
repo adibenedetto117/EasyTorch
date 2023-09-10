39d65192f1d408764944104051420a41fcdae0f3
adibenedetto117 <75105824+adibenedetto117@users.noreply.github.com>
2023-09-10 12:31:37 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+setuptools.setup(
+    name='fastpytorch',
+    version='0.0.1',
+    author='Anthony DiBenedetto and David Abrutis',
+    author_email='adibenedetto117@gmail.com',
+    description='Fast and easy wrapper for PyTorch making it easy to use for beginners',
+    url='https://github.com/adibenedetto117/fastpytorch',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'torch',
+        'torchvision',
+        'numpy'
+    ]
+
+)
